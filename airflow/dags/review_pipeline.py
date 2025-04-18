0fc01e56cf658249e262718ac752ff71325f6673
Michel D.P.D. <micheldpd.ia24@gmail.com>
2025-04-18 19:26:32 +0200

    added topic modeling task  to the pipeline.   the modeling task is run  in a docker container launched using  the dockerOperator

--- airflow/dags/review_pipeline.py
+++ airflow/dags/review_pipeline.py
@@ -1,10 +1,56 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from datetime import datetime, timedelta
 
 from review_manager import ReviewsManager  # Import the ReviewsManager class
+import docker
 
+# ----
+# Function to manage the container running the model training&evaluation and the model's dashboard
+# ----
+
+def check_and_stop_bertopic_container(**kwargs):
+    """
+    Check if bertopic' container is running. If so, stop it and remove it. 
+    otherwise, do nothing.
+    """
+    client = docker.from_env()
+    container_name = 'bertopic'
+    
+    try:
+        # Search the container by name
+        containers = client.containers.list(all=True, filters={"name": container_name})
+        
+        if containers:
+            container = containers[0]
+            container_status = container.status
+            
+            print(f"Container 'bertopic' found with status: {container_status}")
+            
+            # Si le container est en cours d'exécution, l'arrêter
+            if container_status == 'running':
+                print("Stopping bertopic' container ...")
+                container.stop()
+                print("Container 'bertopic' stopped successfully")
+                
+            # Removing the container
+            print("Removing 'bertopic'container ...")
+            container.remove()
+            print("'bertopic' container removed successfully")
+            
+        else:
+            print("No container named 'bertopic' found. Continuing the pipeline.")
+            
+    except Exception as e:
+        print(f"Error during container verification/stopping: {e}")
+        # Do not fail the pipeline in case of error
+        print("pipeline continuation despite the error")
+
+# ----
 
 # Default arguments for the DAG
 default_args = {
@@ -13,7 +59,7 @@ default_args = {
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 3,
+    # 'retries': 3,
     'retry_delay': timedelta(minutes=5),
 }
 
@@ -22,7 +68,7 @@ dag = DAG(
     'reviews_processing_pipeline',
     default_args=default_args,
     description='A pipeline to scrape, process, and analyze customer reviews.',
-    schedule_interval='*/60 * * * *',
+    schedule_interval='*/120 * * * *',
     catchup=False,
 )
 
@@ -70,6 +116,15 @@ def branch_task(**kwargs):
         return 'stop_pipeline_task'
 
 
+# Tâche de vérification et suppression du container si nécessaire
+check_container_task = PythonOperator(
+    task_id='check_and_stop_bertopic_container',
+    python_callable=check_and_stop_bertopic_container,
+    provide_context=True,
+    dag=dag,
+)
+
+
 # Define the tasks using PythonOperator
 extract_reviews = PythonOperator(
     task_id='extract_reviews',
@@ -106,18 +161,25 @@ stop_pipeline_task = DummyOperator(
 )
 
 # Add a python operator for the train model task
-topic_modeling_task = DummyOperator(
+topic_modeling_task = DockerOperator(
     task_id='topic_modeling_task', 
+    image='topic-container:latest',
+    container_name='bertopic',
+    docker_url='unix:///var/run/docker.sock',
+    mounts=[
+       # Mount(source="/var/run/docker.sock", target="/var/run/docker.sock", type="bind"),
+        Mount(source="/Users/micheldpd/Projects/rev_analysis/data/full", target="/data/full", type="bind"),
+        Mount(source="/Users/micheldpd/Projects/rev_analysis/data/output", target="/data/output", type="bind"),
+        Mount(source="/Users/micheldpd/Projects/rev_analysis/topic_modeling", target="/app", type="bind"),
+    ],
+    network_mode='bridge',
+    auto_remove='never',
+    # tty=True,
+    port_bindings={8050: 8051},
     dag=dag,
 )
 
-# Add a dummy operator for the evaluate model task
-# evaluate_model_task = DummyOperator(
-#     task_id='evaluate_model_task',   
-#     dag=dag,
-# )
-
 # Set task dependencies
-extract_reviews >> process_reviews >> load_reviews >> branch
-branch >> [topic_modeling_task, stop_pipeline_task]
-# topic_modeling_task >> evaluate_model_task
+check_container_task >> extract_reviews >> process_reviews >> load_reviews
+load_reviews >> branch >> [topic_modeling_task, stop_pipeline_task]
+
